e6f2feff59b01740b92c3063b802513c608fb69a
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2025-12-26 00:30:34 +0500

    add login page functionality

---
+++ login.js
@@ -0,0 +1,25 @@
+let emailLogin = document.querySelector('#email')
+let passwordLogin = document.querySelector('#password')
+
+document.querySelector('#loginBtn').addEventListener('click', function(){
+    if(!emailLogin.value || !passwordLogin.value){
+        return alert('All the fields are required to fill')
+    }
+    if(passwordLogin.value<8){
+        return alert('Password is not correct')
+    }
+    let allUsers = JSON.parse(localStorage.getItem('allUsers')) || []
+
+    let isExists = allUsers.find(function(userData){
+        return userData.email.toLowerCase() == emailLogin.value.toLowerCase()
+    })
+
+    if(!isExists) return alert('Please create your account first')
+    if(isExists.password == passwordLogin.value){
+        alert('Congratulations, we are signing you up')
+        window.location = 'jobs_page.html'
+    }
+    else{
+        alert('Incorrect password')
+    }
+})
